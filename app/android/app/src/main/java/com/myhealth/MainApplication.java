d5d3b2c47be42294fffcb310d8f632e5c848fe39
cesarmosqueira <cesarmanuel.2802@gmail.com>
2022-08-31 17:05:21 -0500

    hms injected

--- app/android/app/src/main/java/com/myhealth/MainApplication.java
+++ app/android/app/src/main/java/com/myhealth/MainApplication.java
@@ -13,6 +13,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
 import com.myhealth.newarchitecture.MainApplicationReactNativeHost;
+import com.huawei.hms.rn.health.HmsHealthPackage;
 
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+	  packages.add(new HmsHealthPackage());
       return packages;
     }
 
