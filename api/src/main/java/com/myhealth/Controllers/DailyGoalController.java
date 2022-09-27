958913c8e30bb62c4b96f9e49a04a8e6003f9319
diegodba29@gmail.com <drexlarr@DESKTOP-49Q45NV>
2022-09-26 22:13:21 -0500

    endpoint test

--- api/src/main/java/com/myhealth/Controllers/DailyGoalController.java
+++ api/src/main/java/com/myhealth/Controllers/DailyGoalController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/dailyGoal")
+@RequestMapping("/dailyGoalo")
 public class DailyGoalController {
 
 	@Autowired
